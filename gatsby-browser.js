a0b2859ecfc5cd8654017821c86e421ec0e5cd23
antonhansel <antoninribeaud@gmail.com>
2026-03-19 19:25:46 +0100

    Redirect secondary domains to antonin.cool client-side

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -14,3 +14,14 @@ import "prismjs/themes/prism.css"
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js")
 }
+
+// Redirect secondary domains to primary
+const PRIMARY_DOMAIN = "antonin.cool"
+if (
+  typeof window !== "undefined" &&
+  window.location.hostname !== PRIMARY_DOMAIN &&
+  window.location.hostname !== "www." + PRIMARY_DOMAIN &&
+  window.location.hostname !== "localhost"
+) {
+  window.location.replace("https://" + PRIMARY_DOMAIN + window.location.pathname)
+}
